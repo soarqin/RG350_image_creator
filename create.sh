2607b06a639e38b449cdaa963d9bdbc38d517a4b
Soar Qin <soarchin@gmail.com>
2019-11-17 00:37:59 +0800

    fixed create script, UBIBoot sectors are correctly written in now

--- create.sh
+++ create.sh
@@ -5,19 +5,20 @@ source ./env.sh
 
 size=$(((${part1_size}+${part2_size})*1024*2+32))
 
-dd if=/dev/zero of="${filename}" bs=512 count=${size}
+dd if=/dev/zero of="${filename}" oflag=append bs=512 count=${size}
 (echo 32,819168,0xb
 echo 819200,,0x83
 echo write
 ) | sfdisk "${filename}"
 
+dd bs=512 seek=1 if=./data/boot.bin of="${filename}" conv=notrunc
 device=$(losetup -Pf --show "${filename}")
 yes | mkfs.vfat -F32 ${device}p1
 yes | mkfs.ext4 ${device}p2
 
 mkdir -p "${bootdir}"
 mount -o noatime,nodiratime,rw ${device}p1 "${bootdir}"
-cp -Rf "./data/." "${bootdir}/"
+cp -Rf ./data/mininit-syspart*  "${bootdir}/"
 mkdir -p "${bootdir}/dev" "${bootdir}/root"
 
 umount "${bootdir}"
