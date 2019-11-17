29f2fa3edd2a318912bcea4cf795e954ae8999c8
Soar Qin <soarchin@gmail.com>
2019-11-17 23:10:54 +0800

    fixed umount process

--- flasher/src/flasher.sh
+++ flasher/src/flasher.sh
@@ -22,10 +22,10 @@ if [ $? -eq 1 ] ; then
 fi
 
 /sbin/swapoff -a
-/bin/umount /dev/mmcblk0p2 1>/dev/null 2>&1
+/bin/umount -l /dev/mmcblk0p2 1>/dev/null 2>&1
 let n=5
 while [ `/bin/mount | grep mmcblk0p2 | wc -l` -gt 0 -a $n -gt 0 ]; do
-  /bin/umount /dev/mmcblk0p2 1>/dev/null 2>&1
+  /bin/umount -l /dev/mmcblk0p2 1>/dev/null 2>&1
   echo "unmount failed, retrying ($n times left)"
   let n=$n-1
   sleep 1
@@ -34,6 +34,7 @@ if [ $n -eq 0 ]; then
   exit 1
 fi
 
+sync /dev/mmcblk0p2
 (./pv -n __FILENAMEHOLDER__ | ./dd of=/dev/mmcblk0 oflag=sync bs=4M conv=notrunc,noerror) 2>&1 | dialog --gauge "Flashing, please wait..." 7 48 0
 sync /dev/mmcblk0
 
